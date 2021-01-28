a27bea3e6131254df88f2260e91d194a573ab93f
alexey.krikunov <alexey.krikunov@ringcentral.com>
2021-01-28 15:51:02 +0300

    cpu support for vgg19

--- models/networks.py
+++ models/networks.py
@@ -124,8 +124,12 @@ class GANLoss(nn.Module):
 
 class VGGLoss(nn.Module):
     def __init__(self, gpu_ids):
-        super(VGGLoss, self).__init__()        
-        self.vgg = Vgg19().cuda()
+        super(VGGLoss, self).__init__() 
+        if not gpu_ids: 
+            self.vgg = Vgg19().to("cpu")
+        else:
+            self.vgg = Vgg19().cuda()
+
         self.criterion = nn.L1Loss()
         self.weights = [1.0/32, 1.0/16, 1.0/8, 1.0/4, 1.0]        
 
